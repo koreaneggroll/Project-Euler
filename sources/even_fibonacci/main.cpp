bc99efdc520ba94da463115682268873817b2593
koreaneggroll <aringrigoras@gmail.com>
2022-04-10 21:26:44 +0300

    even fibonacci up to 4 milion

--- sources/even_fibonacci/main.cpp
+++ sources/even_fibonacci/main.cpp
@@ -10,13 +10,13 @@ int main(void){
     std::size_t t2 = 1;
     std::size_t t3 = t1 + t2;
 
-    while (t3 <= 100){
+    while (t3 <= MAX_FIBONACCI){
         std::cout << t3 << std::endl;
         t1 = t2;
         t2 = t3;
         t3 = t1 + t2;
 
-        if(t3 % 2 == 0 && t3 <= 100){
+        if(t3 % 2 == 0 && t3 <= MAX_FIBONACCI){
             std::size_t result_before = result;
             result += t3;
             std::cout << result_before << " + " << t3 << " = " << result << std::endl;
